d2e3297a698476e7a63b776f64cd408275aa72d5
amandesai01 <amandesai01@gmail.com>
2024-06-01 14:07:39 +0530

    hosting: use db vars from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactEntity } from './contact.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 55000,
-      username: 'postgres',
-      password: 'postgrespw',
-      database: 'bitespeed',
-      entities: [ContactEntity],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('PG_HOST'),
+        port: parseInt(configService.getOrThrow('PG_PORT')),
+        username: configService.getOrThrow('PG_USERNAME'),
+        password: configService.getOrThrow('PG_PASSWORD'),
+        database: configService.getOrThrow('PG_DATABASE'),
+        entities: [ContactEntity],
+        synchronize: true,
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
